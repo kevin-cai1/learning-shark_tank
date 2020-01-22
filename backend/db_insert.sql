a9a2e78e5b61f44bee57b6b162b13f8ba1fad327
Tamar <tajacobs@deloitte.com.au>
2020-01-22 11:48:36 +1100

    Adding Azure, certificates Tamar

--- backend/db_insert.sql
+++ backend/db_insert.sql
@@ -8,6 +8,7 @@ INSERT INTO Coach VALUES ('coach1@deloitte.com.au', 'Coach 1');
 
 INSERT INTO Course VALUES ('AWS', 'true', 'Specialisation');
 INSERT INTO Course VALUES ('Mulesoft', 'false', 'Specialisation');
+INSERT INTO Course VALUES ('Azure', 'true','Cloud');
 
 INSERT INTO LearningEntry VALUES ('1', 'kevcai@deloitte.com.au', 'AWS', '20-01-2020', '20-07-2020');
 INSERT INTO LearningEntry VALUES ('1','tajacobs@deloitte.com.au','Azure','23-01-2020','23-02-2020');
