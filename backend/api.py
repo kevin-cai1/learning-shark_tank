4dadb2948cb0b80c130468cd4205ae1a7c226a29
Tamar <tajacobs@deloitte.com.au>
2020-01-23 14:35:01 +1100

    learning plan POST request functional

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
 import db
 from datetime import date
 
@@ -11,6 +11,12 @@ api = Api(
         description="API to serve learning-mgmt-system"
         )
 today = date.today().strftime('%d-%m-%Y')
+
+entry_data = api.model('entry',{
+    'task' : fields.String(description = 'Task individual intends to complete i.e. certification, course, exam'),
+    'start_date' : fields.DateTime(description = 'Date individual intends to start the task'),
+    'end_date' : fields.DateTime(description = 'Date individual intends to have completed the task'),
+})
 
 @api.route('/test/<int:number>')
 @api.doc(params={'number': 'test number'})
@@ -96,10 +102,25 @@ class entryFunctions(Resource):
         return user_id
     
     @api.doc(description="TODO Adds new entry")
-    @api.doc(params={'user_id': 'the email address associated with a user','certificate_name':'certificate you would like to complete'})
+    @api.doc(params={'user_id': 'the email address associated with a user'})
+    #@api.response(201, "Created successfully") #Ideally but in most cases 200
+    @api.expect(entry_data)
     def post(self,user_id): # TO DO
-        return user_id
+        req = request.get_json(force=True)
+        conn = db.get_conn() 
+        c = conn.cursor() #cursor to execute commands
+        c.execute('INSERT INTO LearningEntry values(?,?,?,?,?)',(generate_ID(), user_id, req['task'],req['start_date'],req['end_date']),)
+        conn.commit()
+        conn.close()
+        return 'Entry saved'
 
+def generate_ID():
+    conn = db.get_conn() 
+    c = conn.cursor() #cursor to execute commands
+    c.execute('SELECT COUNT(*) FROM LearningEntry')
+    sum = c.fetchone()[0]
+    print(sum)
+    return sum+1
 
 if __name__ == "__main__":
     app.run(debug=True)
